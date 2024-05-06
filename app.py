41ffeed2d7033ab74c3921e0d8be909a04d137d7
Coder Shiyar <60817204+codershiyar@users.noreply.github.com>
2024-05-07 00:36:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('yolov8s.pt')
+model = YOLO('model.pt')
 print(model.names)
 webcamera = cv2.VideoCapture(0)
 # webcamera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
@@ -22,3 +22,37 @@ while True:
 webcamera.release()
 cv2.destroyAllWindows()
 
+# For Realsense camera
+   # def initialize_realsense():
+    #    import pyrealsense2 as rs
+    #    pipeline = rs.pipeline()
+     #   camera_aconfig = rs.config()
+      #  camera_aconfig.enable_stream(rs.stream.depth, *config.DEPTH_CAMERA_RESOLUTION, rs.format.z16, config.DEPTH_CAMERA_FPS)
+     #   camera_aconfig.enable_stream(rs.stream.color, *config.COLOR_CAMERA_RESOLUTION, rs.format.bgr8, config.COLOR_CAMERA_FPS)
+     #   pipeline.start(camera_aconfig)
+      #  return pipeline
+# try:
+#     # Try to initialize RealSense Camera
+#     camera = initialize_realsense()
+#     get_frame = get_frame_realsense
+# except Exception as e:
+#     print("RealSense camera not found, using default webcam.")
+#     camera = initialize_webcam()
+#     get_frame = get_frame_webcam
+
+# Function to get frames from RealSense
+# def get_frame_realsense(pipeline):
+#     import pyrealsense2 as rs
+#     frames = pipeline.wait_for_frames()
+#     depth_frame = frames.get_depth_frame()
+#     color_frame = frames.get_color_frame()
+#     if not depth_frame or not color_frame:
+#         return None, None
+#     depth_image = np.asanyarray(depth_frame.get_data())
+#     color_image = np.asanyarray(color_frame.get_data())
+#     return depth_image, color_image
+
+# # Function to get frame from webcam
+# def get_frame_webcam(cap):
+#     ret, frame = cap.read()
+#     return None, frame if ret else None
